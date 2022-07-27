c259a9404038a5a1cb8070fb436649c977f4dbed
IvanS <ssivon14@gmail.com>
2022-07-27 12:56:08 +0200

    2 banners

--- iogurtum/src/MyComp/start.tsx
+++ iogurtum/src/MyComp/start.tsx
@@ -4,9 +4,24 @@ import logo from '../assets/logo.png';
 import yogur from '../assets/yogur.png';
 import coffee from '../assets/coffee.png';
 import yogur2 from '../assets/yogur2.png';
+import bocata from '../assets/bocata.png';
 
 
 export default function StartMenu() {
+  const [hover, sethover] = React.useState(false);
+  const inside=()=>{
+    sethover(true);
+  };
+  const outside=()=>{
+    sethover(false);
+  };
+  const [hover2, sethover2] = React.useState(false);
+  const inside2=()=>{
+    sethover2(true);
+  };
+  const outside2=()=>{
+    sethover2(false);
+  };
   return (
     <div>
       <div className='start_menu'>
@@ -40,12 +55,22 @@ export default function StartMenu() {
 
         </div>
       </div>
-      <div className='banner' >
+      <div className='banner' onMouseEnter={()=>inside()}
+        onMouseLeave={()=>outside()}>
         <div className='yogur2_div'>
-          <img src={yogur2} className="yogur2"/>
+          <img src={yogur2} className={!hover? 'yogur2' : 'yogur2_hover'} />
         </div>
-        <div className='banner_text'>
+        <div className={!hover? 'banner_text': 'banner_text_hover'}>
           Yogur helado
+        </div>
+      </div>
+      <div className='banner2' onMouseEnter={()=>inside2()}
+        onMouseLeave={()=>outside2()}>
+        <div className='bocata_div'>
+          <img src={bocata} className={!hover2? 'bocata' : 'bocata_hover'} />
+        </div>
+        <div className={!hover2? 'banner_text': 'banner_text_hover'}>
+          Bar y cafetería
         </div>
       </div>
     </div>
